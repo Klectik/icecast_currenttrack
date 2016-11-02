c5a1cd8faf73c195597138c74db2b81f2560657e
Klectik <klectik@openmailbox.org>
2016-11-02 08:59:41 +0100

    Major update

--- refresh.js
+++ refresh.js
@@ -1,49 +1,54 @@
-var seconds = 5;
-var divid = "icecastdiv";
-var url = "/titre.php";
+var refreshid = "refreshdiv";
+var interval = 5; // In seconds
+var url = "icestats.php";
 
-////////////////////////////////
-//
-// Refreshing the DIV
-//
-////////////////////////////////
+var getHttpRequest = function () {
+  var httpRequest = false;
 
-function refreshdiv(){
+  if (window.XMLHttpRequest) { // Mozilla, Safari,...
+    httpRequest = new XMLHttpRequest();
+    if (httpRequest.overrideMimeType) {
+      httpRequest.overrideMimeType('text/xml');
+    }
+  }
+  else if (window.ActiveXObject) { // IE
+    try {
+      httpRequest = new ActiveXObject("Msxml2.XMLHTTP");
+    }
+    catch (e) {
+      try {
+        httpRequest = new ActiveXObject("Microsoft.XMLHTTP");
+      }
+      catch (e) {}
+    }
+  }
 
-// The XMLHttpRequest object
+  if (!httpRequest) {
+    alert('You cannot use AJAX with this browser.');
+    return false;
+  }
 
-var xmlHttp;
-try{
-xmlHttp=new XMLHttpRequest(); // Firefox, Opera 8.0+, Safari
-}
-catch (e){
-try{
-xmlHttp=new ActiveXObject("Msxml2.XMLHTTP"); // Internet Explorer
-}
-catch (e){
-try{
-xmlHttp=new ActiveXObject("Microsoft.XMLHTTP");
-}
-catch (e){
-alert("Your browser does not support AJAX.");
-return false;
-}
-}
-}
-
-// The code...
-xmlHttp.open("GET",url,true);
-xmlHttp.send(null);
-xmlHttp.onreadystatechange=function(){
-if(xmlHttp.readyState==4){
-document.getElementById(divid).innerHTML=xmlHttp.responseText;
-setTimeout('refreshdiv()',seconds*1000);
-}
-}
+  return httpRequest;
 }
 
-// Start the refreshing process
+var xhr = getHttpRequest()
 
-window.onload = function startrefresh(){
-setTimeout('refreshdiv()',seconds*1000);
-}	
+var refresh = function () {
+  xhr.open('GET', url, true);
+  xhr.setRequestHeader('X-Requested-With', 'xmlhttprequest');
+  xhr.send();
+
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4) {
+      if (xhr.status === 200) {
+          document.getElementById(refreshid).innerHTML = xhr.responseText;
+      } else {
+          document.getElementById(refreshid).innerHTML = "Error";
+      }
+    }
+  }
+  setTimeout(refresh, interval*1000);
+}
+
+// Uncomment the following line if the title takes 5 seconds to show, I had this problem with JPlayer
+//setTimeout(refresh, 150);
